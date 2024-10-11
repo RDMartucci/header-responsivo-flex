c72190b98777436ca7f2e10a7413138eaae69268
dario martucci <dariomartucci@gmail.com>
2024-10-11 17:14:51 -0300

    Primer commit

---
+++ menu.js
@@ -0,0 +1,11 @@
+// document.getElementById('hamburguesa').addEventListener('click', function () {
+//     document.querySelector('.header-contenedor').classList.toggle('menu-activo');
+// });
+document.getElementById('hamburguesa').addEventListener('click', function () {
+    document.getElementById('menu-header-top').classList.toggle('menu-top-activo');
+    document.getElementById('menu-principal').classList.toggle('menu-principal-activo');
+});
+
+document.getElementById('icono-busqueda').addEventListener('click',function(){
+    document.querySelector('.form-buscar').classList.toggle('form-buscar-activo');
+});
